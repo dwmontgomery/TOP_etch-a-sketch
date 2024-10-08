72c70946c60f701d13509f57126bf76e2a360f71
Dylan Montgomery <dmont025@odu.edu>
2024-10-08 15:46:06 -0400

    added funciton for changeColor and promptNumBlocks

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 
 const container = document.querySelector("#container");
-// create the divs for the grid
 
+// create the divs for the grid
 function createDivs(numDivs) {
     for (i = 0; i < numDivs; i++) {
         const block = document.createElement("div");
         block.classList.add("block");
+        block.addEventListener("mouseover", changeColor(block));
         container.appendChild(block);
     }
 }
+
+function changeColor(block) {
+    block.style.backgroundColor = "green";
+
+}
+
+function promptNumBlocks() {
+    let numDivs;
+    let text = prompt("Enter the number of blocks.");
+
+}
+
+createDivs(16);
+
+const numButt = document.querySelector("#choose");
+numButt.addEventListener("click", promptNumBlocks);
