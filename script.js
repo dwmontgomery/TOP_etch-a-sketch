dd4a4a9f83e139df80adfb2affd7b620cd36b611
Dylan Montgomery <dmont025@odu.edu>
2024-11-03 18:44:49 -0500

    added random color and opacity functionality

--- script.js
+++ script.js
@@ -12,14 +12,18 @@ function createDivs(numDivs) {
         block.classList.add("block");
         block.style.width = `${blockSize}px`;
         block.style.height = `${blockSize}px`;
+        block.style.opacity = 0;
         block.addEventListener("mouseover", () => changeColor(block));
         container.appendChild(block);
     }
 }
 
 function changeColor(block) {
-    block.style.backgroundColor = "green";
+    block.style.backgroundColor = getRandomColor();
 
+    let currentOpacity = parseFloat(block.style.opacity); 
+    currentOpacity = Math.min(currentOpacity + 0.1, 1); 
+    block.style.opacity = currentOpacity;
 }
 
 function promptNumBlocks() {
@@ -29,4 +33,11 @@ function promptNumBlocks() {
 
 
 numButt.addEventListener("click", promptNumBlocks);
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
